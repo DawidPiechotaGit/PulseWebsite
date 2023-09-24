c61664f96bd4ca61f3f5fa3a7e567d8c1cfbfe02
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-09-24 04:26:46 +0200

    sorting for pdm + styling

--- script-pdm.js
+++ script-pdm.js
@@ -35,3 +35,78 @@ $(".button").click(function () {
   $(".button").removeClass("active");
   $(this).addClass("active");
 });
+
+const sortBtn = document.querySelector(".btn-sort");
+
+function sortProductsPriceAscending() {
+  sortBtn.innerHTML = "PRICE ↑";
+  var gridItems = $(".item");
+  gridItems.sort(function (a, b) {
+    return (
+      $(".vehicle-price", a).data("price") -
+      $(".vehicle-price", b).data("price")
+    );
+  });
+
+  $(".grid").append(gridItems);
+}
+
+function sortProductsPriceDescending() {
+  sortBtn.innerHTML = "PRICE ↓";
+  var gridItems = $(".item");
+  gridItems.sort(function (a, b) {
+    return (
+      $(".vehicle-price", b).data("price") -
+      $(".vehicle-price", a).data("price")
+    );
+  });
+
+  $(".grid").append(gridItems);
+}
+
+const vehicleName = document.querySelectorAll(".vehicle-name");
+
+function sortNameAscending() {
+  sortBtn.innerHTML = "NAME ↑";
+  Array.prototype.slice
+    .call(document.body.querySelectorAll(".item"))
+    .sort(function sort(ea, eb) {
+      var a = ea.textContent.trim();
+      var b = eb.textContent.trim();
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    })
+    .forEach(function (div) {
+      div.parentElement.appendChild(div);
+    });
+}
+
+function sortNameDescending() {
+  sortBtn.innerHTML = "NAME ↓";
+  Array.prototype.slice
+    .call(document.body.querySelectorAll(".item"))
+    .sort(function sort(ea, eb) {
+      var a = ea.textContent.trim();
+      var b = eb.textContent.trim();
+
+      if (a > b) return -1;
+      if (a < b) return 1;
+      return 0;
+    })
+    .forEach(function (div) {
+      div.parentElement.appendChild(div);
+    });
+}
+
+sortNameAscending();
+
+const buttonDown = document.querySelector(".btn-pricedown");
+const buttonUp = document.querySelector(".btn-priceup");
+const buttonNameDown = document.querySelector(".btn-namedown");
+const buttonNameUp = document.querySelector(".btn-nameup");
+
+buttonDown.addEventListener("click", sortProductsPriceAscending);
+buttonUp.addEventListener("click", sortProductsPriceDescending);
+buttonNameDown.addEventListener("click", sortNameAscending);
+buttonNameUp.addEventListener("click", sortNameDescending);
